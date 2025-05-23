bc52909655771a643f0c70d8351090093cfc38c4
shafiul islam laam <rjlam0514@gmail.com>
2025-05-23 15:31:14 +0600

    Helmetprovider setup

--- src/Component/NewPlant.jsx
+++ src/Component/NewPlant.jsx
@@ -26,7 +26,7 @@ const NewPlants = () => {
       }`}
     >
       <Helmet>
-        <title>Mango Grove Tracker | New Plants</title>
+        <title>Mango Grove Tracker | Home</title>
       </Helmet>
       <h2 className="mb-6 text-3xl font-bold text-center text-green-800">
          New Plants
